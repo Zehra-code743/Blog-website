f0851e0e1f24d7a57b25f3a450bb8085e902c085
Shan-e-Zehra <shanezehra117@gmail.com>
2025-01-05 12:09:34 +0500

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "@/components/footer"
-
-
-
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,11 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      
-    <Footer/>
-      {children}
-    
-    
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
